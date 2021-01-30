b17a74f8cf94bc22e059bb145c117b6d411fcb14
Jose Santos <jsantos108@gmail.com>
2021-01-30 14:07:12 -0800

    Added password encryption

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,52 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.post('/register', (req, res) => {
-    User.findOne({email: req.body.email}, (err, obj) => {
+    User.findOne({email: req.body.email}, (err, user) => {
         if(err){
             res.send(err);
         }else {
-            if(obj){
+            if(user){
                 res.send("Someone is already using that email");
             }else {
-                const newUser = new User({
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password
+                bcrypt.hash(req.body.password, saltRounds, function(err, hashedPassword) {
+                    const newUser = new User({
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: hashedPassword
+                    });
+    
+                    newUser.save()
+                        .then(() => res.send('User Created'))
+                        .catch(err => res.status(400).json(err));
                 });
 
-                newUser.save()
-                    .then(() => res.send(`User Created`))
-                    .catch(err => res.status(400).json(err));
+                
             }
         }
     });
 })
+
+router.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(err){
+            res.send(err);
+        }else {
+            if(!user){
+                res.send("Email is incorrect");
+            }else {
+                bcrypt.compare(req.body.password, user.password, function(err, result) {
+                    if(result) {
+                        res.send('Logged In');
+                    }else {
+                        res.send('Password is incorrect')
+                    }
+                });
+            }
+        }
+    });
+});
 
 module.exports = router;
